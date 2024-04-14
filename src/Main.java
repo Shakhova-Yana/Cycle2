ead4e3df26e78498927d8f2a6509bdc00aee10c2
НафНафДурикович <yana88yana99@gmail.com>
2024-04-14 19:21:20 +0300

    Здание 1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@ public class Main {
     public static void main(String[] args) {
 //task1
         System.out.println("\ntask 1");
-
+        int money = 0;
+        int month = 1;
+        while (money < 2_459_000) {
+            money += 15_000;
+            System.out.printf("Месяц %s, сумма накоплений равна %s рублей%n", month, money);
+            month++;
+        }
+//task2
+        System.out.println("\ntask 2");
     }
 }
